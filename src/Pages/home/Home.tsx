4c4ef9c47ea2d79af31556f5b284a3e1a06d7e22
‘Lekky <lekkyjay@yahoo.com>
2023-03-28 16:37:39 +0200

    Products component created.

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
 import Categories from '../../components/categories/Categories'
 import Category from '../../components/category/Category'
+import Products from '../../components/products/Products'
 import Slider from '../../components/slider/Slider'
 import { 
   fetchCategories, fetchProductsByCategory, selectAllCategories, selectAllProductsByCategory, selectCategoryAllStatus, selectCategoryStatus 
 } from '../../redux/categorySlice'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+import { fetchProducts } from '../../redux/productSlice'
 import './Home.scss'
 
 export default function Home() {
@@ -14,9 +16,10 @@ export default function Home() {
   const categoryStatus = useAppSelector(selectCategoryStatus)
   const productsByCategory = useAppSelector(selectAllProductsByCategory)
   const catProductAllStatus = useAppSelector(selectCategoryAllStatus)
+  const {data: products, status: productStatus} = useAppSelector((state) => state.product)
 
   useEffect(() => {
-    // dispatch(fetchProducts())
+    dispatch(fetchProducts())
     dispatch(fetchCategories())
     dispatch(fetchProductsByCategory(1, 'all'))
     dispatch(fetchProductsByCategory(2, 'all'))
@@ -26,6 +29,7 @@ export default function Home() {
     <div>
       <Slider />
       <Categories categories={categories} status={categoryStatus} />
+      <Products products={products} status={productStatus} />
       <section>
         { productsByCategory[0] && <Category products={productsByCategory[0]} status={catProductAllStatus} /> }
       </section>
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+import { setModalData, setIsModalVisible } from '../../redux/modalSlice'
+import { formatPrice } from '../../utils/helpers'
+import { STATUS } from '../../utils/status'
+import Loader from '../loader/Loader'
+import Error from '../error/Error'
+import ProductDetails from '../product/ProductDetails'
+import { IProduct } from '../../utils/interfaces'
+import './Products.scss'
 
-export default function Products() {
+interface IProps {
+  products: IProduct[]
+  status: string
+}
+
+export default function Products({products, status}: IProps) {
+  const dispatch = useAppDispatch()
+  const {isModalVisible} = useAppSelector((state) => state.modal)
+
+  const viewModalHandler = (data: IProduct) => {
+    dispatch(setModalData(data))
+    dispatch(setIsModalVisible(true))
+  }
+
+  if(status === STATUS.ERROR) return (<Error />)
+  if(status === STATUS.LOADING) return (<Loader />)
+
   return (
-    <div>Products</div>
+    <section className='product py-5 bg-ghost-white' id = "products">
+      { isModalVisible && <ProductDetails />}
+
+      <div className='container'>
+        <div className='product-content'>
+          <div className='section-title'>
+            <h3 className='text-uppercase fw-7 text-regal-blue ls-1'>Our Products</h3>
+          </div>
+          <div className='product-items grid'>
+            {
+              products.slice(0, 20).map(product => (
+                <div className='product-item bg-white' key = {product.id} onClick = {() => viewModalHandler(product)}>
+                  <div className='product-item-img'>
+                    <img src = {product.images[0]} alt = "" />
+                    <div className = "product-item-cat text-white fs-13 text-uppercase bg-gold fw-6">{product.category.name}</div>
+                  </div>
+                  <div className='product-item-body'>
+                    <h6 className = "product-item-title text-pine-green fw-4 fs-15">{product.title}</h6>
+                    <div className = "product-item-price text-regal-blue fw-7 fs-18">{formatPrice(product.price)}</div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
